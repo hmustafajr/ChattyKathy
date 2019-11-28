a0a201ae8cb2a7aa76cc1961f40de336e6f1b88e
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-11-27 22:15:04 -0800

    webHook Listener
    wbHook Listener with status resend to support Postman

---
+++ webHookListener.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const EventEmitter = require('events');
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+app.use(bodyParser.json());
+
+class WebhookListener extends EventEmitter {
+  listen() {
+    app.post('/kofi', (req, res) => {
+      const data = req.body.data;
+      const { message, timestamp } = data;
+      const amount = parseFloat(data.amount);
+      const senderName = data.from_name;
+      const paymentId = data.message_id;
+      const paymentSource = 'Ko-fi';
+
+      //the OK status is intended for me to observe in Postman
+	  //ko-fi disregards response body and only observes 200 status
+      res.send({ status: 'OK' });
+
+      this.emit(
+        'donation',
+        paymentSource,
+        paymentId,
+        timestamp,
+        amount,
+        senderName,
+        message,
+      );
+    });
+
+    app.listen(PORT);
+  }
+}
+
+const listener = new WebhookListener();
+listener.listen();
+
+module.exports = listener;
